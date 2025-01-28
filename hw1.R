6152e34c2079d51eb8505ba50c2eef08073dfb82
sarahogden2017 <sarahogden2017@gmail.com>
2025-01-28 09:39:10 -0500

    hw1

---
+++ hw1.R
@@ -0,0 +1,95 @@
+# Sarah Ogden
+# STA305 HW 1
+
+library(datasets)
+library(FSA)
+library(ggplot2)
+library(dplyr)
+
+data("mtcars")
+
+# Cast values w limited number of values as factors
+cardata = mtcars
+cardata$cyl_F = as.factor(cardata$cyl)
+cardata$gear_F = as.factor(cardata$gear)
+cardata$vs_F = factor(cardata$vs,labels=c("V-shaped", "straight"))
+cardata$am_F = factor(mtcars$am,labels=c("automatic","manual"))
+
+# 3a
+# overall
+mean(cardata$hp)
+sd(cardata$hp)
+# by cylinders
+aggregate(cardata$hp, list(cardata$cyl_F), FUN=mean)
+
+# 3b
+# overall
+summary(cardata$mpg)
+# by transmission
+Summarize(mpg ~ am_F, data=cardata)
+
+# 3c
+XT = xtabs( ~ cyl_F , data=cardata)
+XT
+prop.table(XT)
+
+# 3d
+XT = xtabs( ~ vs_F + am_F, data=cardata)
+XT
+
+# 4
+# graph 1 - pie graph of engine types
+data <- data.frame(xtabs( ~ vs_F, data=cardata))
+# Compute the position of labels
+data <- data %>% 
+  arrange(desc(vs_F)) %>%
+  mutate(prop = Freq / sum(Freq) *100) %>%
+  mutate(ypos = cumsum(prop)- 0.5*prop )
+data
+ggplot(data, aes(x="",y=prop,fill=vs_F)) +
+  geom_bar(stat="identity",width=1) +
+  coord_polar("y",start=0) +
+  theme_void() +
+  labs(fill='Engine Type') +
+  geom_text(aes(y = ypos, label=paste0(prop, "%")))
+
+# graph 2
+data <- data.frame(xtabs( ~ cyl_F, data=cardata))
+ggplot(data, aes(x=cyl_F,y=Freq,fill="")) +
+  geom_bar(stat="identity") +
+  theme(legend.position="none") +
+  labs(x="Number of Cylinders",y="Amount of Cars")
+
+# graph 3
+ggplot(cardata, aes(x = carb)) +
+  geom_dotplot(dotsize = 1) +
+  labs(y="Frequency (%)",x="Number of Carburetors in Engine")
+
+# graph 4
+ggplot(cardata, aes(y=mpg)) +
+  geom_boxplot() +
+  labs(y="MPG")
+
+# 5
+# graph 1
+ggplot(cardata, aes(x=hp,y=qsec,color=gear)) +
+  geom_point() +
+  labs(x="Horse Power", y="1/4 Mile Time")
+
+# graph 2
+ggplot(cardata, aes(x=hp,y=qsec,color=vs_F)) +
+  geom_point() +
+  labs(x="Horse Power", y="1/4 Mile Time", color="Engine Shape")
+
+
+# graph 3
+ggplot(cardata, aes(x=hp,y=mpg,size=wt)) +
+  geom_point() +
+  labs(x="Horsepower", y="MPG", size="Weight (1000lbs)")
+
+
+# graph 4
+ggplot(cardata, aes(x=hp,y=mpg,color=am_F)) +
+  geom_point() +
+  labs(x="Horsepower",y="MPG",color="Transmission")
+
--- topic_2_in_class.R
+++ topic_2_in_class.R
@@ -1,17 +1,49 @@
 library(datasets)
 data("mtcars")
 
-## Question 1
+# Question 1
 head(mtcars, n=10)
 tail(mtcars, n=10)
 
-## Question 2
+# Question 2 - Variable types
 sapply(mtcars, class)
 
-## Cast Engine, Transmission, and Cylinders as factors
+# Cast values w limited number of values as factors
 cardata = mtcars
 cardata$cyl_F = as.factor(cardata$cyl)
+cardata$gear_F = as.factor(cardata$gear)
 cardata$vs_F = factor(cardata$vs,labels=c("V-shaped", "straight"))
 cardata$am_F = factor(mtcars$am,labels=c("automatic","manual"))
 
-## Question 3
+# Question 3 - Appropriate descriptive stat
+# mpg: mean, sd
+mean(cardata$mpg)
+sd(cardata$mpg)
+
+# cyl: mode
+mode(cardata$cyl)
+
+# disp: median, IQR
+median(cardata$disp)
+quartile()
+
+# hp: mean, standard deviation
+
+# drat: mean, standard deviation
+
+# wt: mean, standard deviation
+
+# qsec: mean, standard deviation
+
+# vs: mode
+
+# am: mode
+
+# gear: median, IQR
+
+# carb: median, IQR
+
+
+# Question 4 - 4 Univariate graphs
+
+# Question 5 - 4 Multivariate graphs
